84da43323aff6ad39188f2df9cac5d347815289b
krystal <krystal.mark@student.moringaschool.com>
2023-06-12 15:11:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if(feet <= 400){
+    return 'This one is on me!'
+  } else {(feet > 2000)
+    return 'I will gladly take your thirty bucks.'
+  } 
+}
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return 'Ok, sounds good.'
+  } else {(city != 'NYC')
+    return'No go.'
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous'){
+    return 'Thank you so much.'
+  } else {(tip != 'generous')
+    return 'Thank you.'    
+  }
   // Write your code here!
 }
